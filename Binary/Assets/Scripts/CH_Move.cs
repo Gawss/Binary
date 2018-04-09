e20ad24b1751aeb3a9a1c99c93e42d69499dece9
LUIS_MIGUEL_PALACIO_RESTREPO <lpalacio@todo1.net>
2018-04-09 16:55:16 -0500

    GetAxis-ChMovement

--- Binary/Assets/Scripts/CH_Move.cs
+++ Binary/Assets/Scripts/CH_Move.cs
@@ -4,33 +4,33 @@ using UnityEngine;
 
 public class CH_Move : MonoBehaviour {
 
-    public float speed;
-
-
+    float speed;
+	Rigidbody2D rb;
     void Start ()
     {
-
+		rb = GetComponent<Rigidbody2D>();
     }
 
 	void Update(){
-
-		if (Input.GetKey(KeyCode.LeftArrow)){
-			this.transform.position += new Vector3(-0.2f, 0, 0);
-		}
-		if (Input.GetKey(KeyCode.RightArrow)){
-			this.transform.Translate(0.2f, 0, 0);
-		}
-		if (Input.GetKey(KeyCode.DownArrow)){
-			this.transform.localScale = new Vector3(2, 2.5f, 2);
-			if(this.transform.position.y > -0.6f){
-				this.transform.position -= new Vector3(0, 0.2f, 0);
-			}
-		}
-		if (Input.GetKeyUp (KeyCode.DownArrow)){
-			this.transform.localScale = new Vector3(2, 5, 2);
-		}
-		if (Input.GetKey(KeyCode.UpArrow)){
-			this.transform.position += new Vector3(0, 0.2f, 0);
-		}
+		speed = Input.GetAxis("Horizontal");
+		rb.velocity = new Vector3(speed*1.2f, 0, 0);
+		// if (Input.GetKey(KeyCode.LeftArrow)){
+		// 	this.transform.position += new Vector3(-0.2f, 0, 0);
+		// }
+		// if (Input.GetKey(KeyCode.RightArrow)){
+		// 	this.transform.Translate(0.2f, 0, 0);
+		// }
+		// if (Input.GetKey(KeyCode.DownArrow)){
+		// 	this.transform.localScale = new Vector3(2, 2.5f, 2);
+		// 	if(this.transform.position.y > -0.6f){
+		// 		this.transform.position -= new Vector3(0, 0.2f, 0);
+		// 	}
+		// }
+		// if (Input.GetKeyUp (KeyCode.DownArrow)){
+		// 	this.transform.localScale = new Vector3(2, 5, 2);
+		// }
+		// if (Input.GetKey(KeyCode.UpArrow)){
+		// 	this.transform.position += new Vector3(0, 0.2f, 0);
+		// }
 	}
 }
