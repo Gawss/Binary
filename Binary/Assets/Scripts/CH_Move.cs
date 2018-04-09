f187a00f60ad9efbbe27a0e3c21732d43793028a
LUIS_MIGUEL_PALACIO_RESTREPO <lpalacio@todo1.net>
2018-04-09 14:07:02 -0500

    playerMovement

---
+++ Binary/Assets/Scripts/CH_Move.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CH_Move : MonoBehaviour {
+
+    public float speed;
+
+
+    void Start ()
+    {
+
+    }
+
+	void Update(){
+
+		if (Input.GetKey(KeyCode.LeftArrow)){
+			this.transform.position += new Vector3(-0.2f, 0, 0);
+		}
+		if (Input.GetKey(KeyCode.RightArrow)){
+			this.transform.Translate(0.2f, 0, 0);
+		}
+		if (Input.GetKey(KeyCode.DownArrow)){
+			this.transform.localScale = new Vector3(2, 2.5f, 2);
+			if(this.transform.position.y > -0.6f){
+				this.transform.position -= new Vector3(0, 0.2f, 0);
+			}
+		}
+		if (Input.GetKeyUp (KeyCode.DownArrow)){
+			this.transform.localScale = new Vector3(2, 5, 2);
+		}
+		if (Input.GetKey(KeyCode.UpArrow)){
+			this.transform.position += new Vector3(0, 0.2f, 0);
+		}
+	}
+}
