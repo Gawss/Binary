7f3c6b76671e1f5a2db1dd111d2d10a0838fde93
LUIS_MIGUEL_PALACIO_RESTREPO <lpalacio@todo1.net>
2018-04-11 11:08:05 -0500

    jump_CH-anim

--- Binary/Assets/Scripts/CH_Move.cs
+++ Binary/Assets/Scripts/CH_Move.cs
@@ -25,43 +25,33 @@ public class CH_Move : MonoBehaviour {
 		// // rb.velocity = new Vector3(speed_x*5.0f, speed_y*25.0f, 0);
 		// rb.velocity = new Vector3(speed_x*5.0f, speed_y, 0);
 
-		if(Input.GetAxis("Horizontal") != 0){
-			// Debug.Log("animCh = 1");
-			animator.SetInteger("animCh", 1);
-			if(Input.GetAxis("Horizontal") < 0){
-				this.transform.rotation = Quaternion.Euler(0.0f, 180.0f, 0.0f); 
-			}else if(Input.GetAxis("Horizontal") > 0){
-				this.transform.rotation = Quaternion.Euler(0.0f, 0.0f, 0.0f); 
-			}
+
+		if(Input.GetAxis("Horizontal") < 0){
+			Debug.Log("< 0");
+			this.transform.rotation = Quaternion.Euler(0.0f, 180.0f, 0.0f); 
+		}else if(Input.GetAxis("Horizontal") > 0){
+			Debug.Log("> 0");
+			this.transform.rotation = Quaternion.Euler(0.0f, 0.0f, 0.0f); 
 		}
+		
 		else{
 			// Debug.Log("animCh = 0");
 			animator.SetInteger("animCh", 0);
 		}
 
-		// if (Input.GetKey(KeyCode.LeftArrow)){
-		// 	this.transform.position += new Vector3(-0.2f, 0, 0);
-		// }
-		// if (Input.GetKey(KeyCode.RightArrow)){
-		// 	this.transform.Translate(0.2f, 0, 0);
-		// }
-		// if (Input.GetKey(KeyCode.DownArrow)){
-		// 	this.transform.localScale = new Vector3(2, 2.5f, 2);
-		// 	if(this.transform.position.y > -0.6f){
-		// 		this.transform.position -= new Vector3(0, 0.2f, 0);
-		// 	}
-		// }
-		// if (Input.GetKeyUp (KeyCode.DownArrow)){
-		// 	this.transform.localScale = new Vector3(2, 5, 2);
-		// }
 		if(jumping == false){
-			
+
+			if(Input.GetAxis("Horizontal") != 0){
+				// Debug.Log("animCh = 1");
+				animator.SetInteger("animCh", 1);
+			}
 			rb.velocity = new Vector3(speed_x*10.0f, 0, 0);
 			if (Input.GetKey(KeyCode.UpArrow)){
 				// this.transform.position += new Vector3(0, 0.2f, 0);
 				rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
 				// rb.velocity = new Vector2(0, 100.0f);
 				Debug.Log("Player is jumping...");
+				animator.SetInteger("animCh", 2);
 				jumping = true;
 			}
 
@@ -84,6 +74,7 @@ public class CH_Move : MonoBehaviour {
     void OnCollisionEnter2D(Collision2D coll) {
         if (coll.gameObject.tag == "platform")
 			Debug.Log("Player is in the ground...");
+			animator.SetInteger("animCh", 0);
             jumping  = false;
     }
 
