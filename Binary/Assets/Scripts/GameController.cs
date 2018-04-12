cfd624caa45f5ddb13ec0d91029884d01049c0db
LUIS_MIGUEL_PALACIO_RESTREPO <lpalacio@todo1.net>
2018-04-12 12:16:56 -0500

    lampTutorialState

--- Binary/Assets/Scripts/GameController.cs
+++ Binary/Assets/Scripts/GameController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class GameController : MonoBehaviour {
 
 	public static string enemyAnim_State;
+	public static string lampState;
 	public static int level;
 	public static bool initLevel;
 	public GameObject enemyPrefab;
@@ -18,9 +19,11 @@ public class GameController : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		if(initLevel == false){
-			for(int generator = 0; generator < level; generator ++){
-				enemyAnim_State = "alive";
-				Instantiate(enemyPrefab, new Vector3(-7.42f+generator*10, 0.87f, 9.34f), Quaternion.identity);
+			if(level == 99){
+				for(int generator = 0; generator < 2; generator ++){
+					enemyAnim_State = "alive";
+					Instantiate(enemyPrefab, new Vector3(-7.42f+generator*10, 0.87f, -0.7f), Quaternion.identity);
+				}
 			}
 			initLevel = true;
 		}
---
+++ Binary/Assets/Scripts/lampController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lampController : MonoBehaviour {
+
+	public GameObject lampTutorial;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(GameController.lampState == "lampTutorial_On"){
+			lampTutorial.SetActive(true);
+		}else{
+			lampTutorial.SetActive(false);
+		}
+	}
+}
